66285bc0510a90e23a8fc4b1f4c1f955657c3c8b
Matthew Wright <mawright@lbl.gov>
2025-08-01 14:09:33 -0700

    update imports

--- sparse_transformer_layers/blocks/__init__.py
+++ sparse_transformer_layers/blocks/__init__.py
@@ -1,8 +1,12 @@
 from .ms_deform_attn import SparseMSDeformableAttentionBlock
-from .neighborhood_attn import SparseNeighborhoodAttentionBlock
+from .neighborhood_attn import (
+    SparseNeighborhoodAttentionBlock,
+    get_multilevel_neighborhoods,
+)
 from .self_attn import MultilevelSelfAttentionBlockWithRoPE
 
 __all__ = [
+    "get_multilevel_neighborhoods",
     "MultilevelSelfAttentionBlockWithRoPE",
     "SparseMSDeformableAttentionBlock",
     "SparseNeighborhoodAttentionBlock",
